f1619ad3125e3d10e9fc93dfd949804e46db2d3e
Praveen Vaddi <39093812+vaddi2023@users.noreply.github.com>
2025-08-07 12:46:36 +0530

    Delete construction_ai_app directory

--- construction_ai_app/frontend/src/screens/TaskDashboardScreen.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import { View, Text } from "react-native";
-
-const TaskDashboardScreen = () => (
-  <View><Text>TaskDashboardScreen screen placeholder</Text></View>
-);
-export default TaskDashboardScreen;
