6c678a04aee4f47b443d7c3d3ecc72b4089fd04e
Praveen Vaddi <39093812+vaddi2023@users.noreply.github.com>
2025-08-13 13:15:39 +0530

    Delete construction_ai_ directory

--- construction_ai_/frontend/src/screens/LoginScreen.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import { View, Text } from "react-native";
-
-const LoginScreen = () => (
-  <View><Text>LoginScreen screen placeholder</Text></View>
-);
-export default LoginScreen;
